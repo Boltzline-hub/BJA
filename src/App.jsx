22deaf2fddda870e105d841acec7e1577f830484
Boltzline <auguste542lavi@gmail.com>
2023-10-21 14:30:39 +0100

    14:30

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import React from "react";
+import { Element } from 'react-scroll';
 import {Home, About, Portfolio, Contact, Team, Footer, Testimonials, Navba} from './components/index';
 
 const App = () => {
 
-
   return (
     <div>
-    <Navba/> 
-    <Home/>
-    <About/>
-    <Portfolio/>
-    <Team/>
-    <Testimonials/>
-    <Contact/>
+    <Navba/>
+      <Element name="home">
+        <Home />
+      </Element> 
+      <Element name="about" >
+        <About />
+      </Element>
+      <Element name="portfolio">
+        <Portfolio />
+      </Element>
+      <Team/>
+      <Element name="testimonials">
+        <Testimonials />
+      </Element>
+      <Element name="contact">
+        <Contact />
+      </Element>
     <Footer/>
     </div>
   );
